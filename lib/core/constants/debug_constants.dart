bc4e95a1386bea14397682f804f5c4d0b9bf8f3f
armagangok <1armagangok@gmail.com>
2025-03-06 02:40:47 +0300

    bum

--- lib/core/constants/debug_constants.dart
+++ lib/core/constants/debug_constants.dart
@@ -1,12 +1,12 @@
 abstract final class KDebug {
   // enables debug mode for print
-  static const debugModeEnabled = true;
+  static const debugModeEnabled = false;
 
   // enables rate debug mode for print
   static const rateDebugMode = false;
 
   // user becomes a pro member for a debug mode to test the app
-  static const purchaseDebugMode = true;
+  static const purchaseDebugMode = false;
 
   // enables onboarding debug mode for print
   static const onboardingDebugMode = false;
