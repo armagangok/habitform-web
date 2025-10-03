74a25a49ac4fa412f5a4e83cb3dda402409e08a4
armagangok <1armagangok@gmail.com>
2025-10-04 02:45:18 +0300

    bum

---
+++ ios/HabitWidget/HabitWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  HabitWidgetBundle.swift
+//  HabitWidget
+//
+//  Created by Armagan Gok on 2.10.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HabitWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HabitWidget()
+        HabitWidgetControl()
+    }
+}
---
+++ ios/TestWidget/TestWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  TestWidgetBundle.swift
+//  TestWidget
+//
+//  Created by Armagan Gok on 2.10.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TestWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TestWidget()
+        TestWidgetControl()
+    }
+}
